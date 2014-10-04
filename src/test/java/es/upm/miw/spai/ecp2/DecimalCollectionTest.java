2b64886b1571b9b9bde510317b8a84d38a3bded8
robinparadise <robinchogiles@gmail.com>
2014-10-04 12:31:26 +0200

    Tests for DecimalCollection Class

---
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -0,0 +1,33 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+
+	private DecimalCollection decColl;
+
+	@Before
+	public void before() {
+        decColl = new DecimalCollection();
+        decColl.add(1.1);
+        decColl.add(1.2);
+    }
+
+	@Test
+	public final void testSize() {
+		assertEquals(2, decColl.size());
+	}
+
+	@Test
+	public final void testSum() {
+		assertEquals(2.3, decColl.sum(), 10e-5);
+	}
+
+	@Test
+	public final void testHigher() {
+		assertEquals(2.3, decColl.higher(), 10e-5);
+	}
+}
