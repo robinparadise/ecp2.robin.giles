0b74e41a0c4bfa5053773119e46ccfd1eca5fa8a
Carlos Huguet <huguet10@gmail.com>
2014-10-05 19:50:41 +0200

    Ampliación de la Clase UserTest #2 Finalizado

--- src/main/java/es/upm/miw/spai/ecp2/User.java
+++ src/main/java/es/upm/miw/spai/ecp2/User.java
@@ -37,4 +37,9 @@ public class User {
     public String initials() {
         return this.name.substring(0, 1) + ".";
     }
+
+    public String nameUppercase() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
--- src/test/java/es/upm/miw/spai/ecp2/UserTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -1,45 +1,53 @@
 package es.upm.miw.spai.ecp2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
-	
-	private int number = 2;
+
+    private int number = 2;
+
     private String name = "Fulanito";
+
     private String familyName = "Smith";
+
     private User user;
-    
+
     @Before
     public void before() {
         user = new User(number, name, familyName);
     }
 
-	@Test
-	public void testGetNumber() {
-		assertEquals(number, user.getNumber());
-	}
+    @Test
+    public void testGetNumber() {
+        assertEquals(number, user.getNumber());
+    }
 
-	@Test
-	public void testGetName() {
-		assertEquals(name, user.getName());
-	}
+    @Test
+    public void testGetName() {
+        assertEquals(name, user.getName());
+    }
 
-	@Test
-	public void testGetFamilyName() {
-		assertEquals(familyName, user.getFamilyName());
-	}
+    @Test
+    public void testGetFamilyName() {
+        assertEquals(familyName, user.getFamilyName());
+    }
 
-	@Test
-	public void testFullName() {
-		assertEquals("Fulanito Smith", user.fullName());
-	}
+    @Test
+    public void testFullName() {
+        assertEquals("Fulanito Smith", user.fullName());
+    }
 
-	@Test
-	public void testInitials() {
-		assertEquals("F.", user.initials());
-	}
+    @Test
+    public void testInitials() {
+        assertEquals("F.", user.initials());
+    }
+
+    @Test
+    public void nameUppercaseTest() {
+        assertEquals("FULANITO", user.nameUppercase());
+    }
 
 }
