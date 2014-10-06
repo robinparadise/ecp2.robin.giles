6c0cfaae6b240244a00f1684c8b104e544993f76
David <David@netbook-HP>
2014-10-06 19:47:30 +0200

    Ampliacion clase User

--- src/main/java/es/upm/miw/spai/ecp2/User.java
+++ src/main/java/es/upm/miw/spai/ecp2/User.java
@@ -39,7 +39,6 @@ public class User {
     }
 
     public String nameUppercase() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.name.toUpperCase();
     }
 }
